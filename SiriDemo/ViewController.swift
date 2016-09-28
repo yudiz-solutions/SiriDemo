65d61618fba78812718f909a12da3939f22438f4
Ravib <ravib@clariontechnologies.co.in>
2016-09-28 18:22:42 +0530

    First Commit

---
+++ SiriDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SiriDemo
+//
+//  Created by Yudiz Solutions Pvt. Ltd. on 23/09/16.
+//  Copyright © 2016 Yudiz Solution Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
